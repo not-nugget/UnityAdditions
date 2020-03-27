e40413ac048e884c6a9b60ae92654ddb358a31ea
Joshua Favre <jfavre390@gmail.com>
2020-03-27 16:58:58 -0400

    Added test  material and finished off vector2 and 3 extensions

---
+++ Assets/Scripts/PositionRandomizer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityAdditions.Extensions;
+
+public class PositionRandomizer : MonoBehaviour
+{
+    public Collider Collider { get; set; }
+
+    private void Update()
+    {
+        Vector3 n = new Vector3();
+        n.RandomizeWithin(Collider.bounds);
+        transform.position = n;
+    }
+}
---
+++ Assets/Scripts/RandomizerSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizerSpawner : MonoBehaviour
+{
+    public GameObject spawn;
+    public int count = 100;
+
+    private void Awake()
+    {
+        for (int i = 0; i < count; i++) Instantiate(spawn, transform).GetComponent<PositionRandomizer>().Collider = GetComponent<Collider>();
+    }
+
+}
