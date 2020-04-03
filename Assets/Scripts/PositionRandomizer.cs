6b02e14a9cf719c57255e7d1786e70fe8b0faa8e
Joshua Favre <jfavre390@gmail.com>
2020-04-02 21:32:07 -0400

    Update .gitignore

---
+++ Assets/Scripts/PositionRandomizer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityAdditions.Vector3;
+
+public class PositionRandomizer : MonoBehaviour
+{
+    public Collider Collider { get; set; }
+
+    private void Update()
+    {
+        Vector3 n = new Vector3();
+        n.RandomizeWithin(Collider.bounds);
+        transform.position = n;
+    }
+}
